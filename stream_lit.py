0c15a9d152493764a7ed5be20b85960fc640346d
Pranav Chidurala <64633775+Pranav-space@users.noreply.github.com>
2022-09-17 14:54:05 +0530

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -7,3 +7,9 @@ streamlit.header('Breakfast Menu')
 streamlit.text('1.😀Omega 3 & Blueberry Oatmeal')
 streamlit.text('2.😄Kale, Spinach & Rocket Smoothie')
 streamlit.text('3.🎂Hard-Boiled Free-Range Egg')
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
