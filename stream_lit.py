be8215f842d60f3939aad502feac46874c8137a1
Pranav Chidurala <64633775+Pranav-space@users.noreply.github.com>
2022-09-17 14:28:33 +0530

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.title('My Streamlit App')
 streamlit.header('How to build Stram Lit Apps ?')
 streamlit.text('1. Sign into Stream Lit account through GitHub')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
