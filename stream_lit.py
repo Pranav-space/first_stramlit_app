1d57f7b36ba350e35ef987f10158b9f902c45b21
Pranav Chidurala <64633775+Pranav-space@users.noreply.github.com>
2022-09-20 12:31:04 +0530

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -23,8 +23,10 @@ streamlit.dataframe(fruits_to_show)
 #New section to display FruityVice API Response
 streamlit.header('Fruityvice Fruity Advice!')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
